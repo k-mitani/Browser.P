aab165da4aaf3929f1871bad31bfde0a09fcb920
i09158knct <i09158knct@gmail.com>
2020-11-03 16:41:27 +0900

    不要ファイル削除など

---
+++ app/src/main/java/net/i09158knct/android/browserp/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.i09158knct.android.browserp.activities
+
+import android.app.Activity
+import android.os.Bundle
+import net.i09158knct.android.browserp.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
